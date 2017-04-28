b4d9439c842ea303558be7fc8b1ed2ed1866434c
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-04-28 20:56:50 +0200

    printPrimes test and findPrimes

--- src/main/NumberGenerator.java
+++ src/main/NumberGenerator.java
@@ -10,4 +10,20 @@ public class NumberGenerator {
 		}
 		return numbers;
 	}
+	
+	public static ArrayList<String> findPrimes(ArrayList<String> numbers) {
+		for(int i = 0; i < numbers.size(); i++) {
+			Integer num = Integer.parseInt(numbers.get(i));
+			if(isPrime(num)) numbers.set(i, "prime");
+		}
+		return numbers;
+	}
+	
+	private static boolean isPrime(int n) {
+	    for(int i=2;i<n;i++) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}
 }
--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -22,5 +22,30 @@ public class Tests {
 		
 		assertEquals(numbersToTest, numbers);
 	}
+	
+	@Test
+	public void printPrimes() {
+		ArrayList<String> numbers = new ArrayList<String>();
+		final int min = 1, max = 100;
+		for(Integer i = min; i < max; i++) {
+			if(isPrime(i)) numbers.add("prime");
+			else numbers.add(i.toString());
+		}
+		System.out.println(numbers);
+		
+		NumberGenerator numberGenerator = new NumberGenerator();
+		ArrayList<String> numbersToTest = numberGenerator.generate(min, max);
+		ArrayList<String> numbersWithPrimes = numberGenerator.findPrimes(numbersToTest);
+		
+		assertEquals(numbers, numbersWithPrimes);
+	}
+	
+	boolean isPrime(int n) {
+	    for(int i=2;i<n;i++) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}
 
 }

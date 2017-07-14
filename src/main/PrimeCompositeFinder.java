8a541ae060c0478c03d6fd4c00e8e044a238c2a7
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-07-15 00:22:06 +0200

    generateIntegerArray

--- src/main/PrimeCompositeFinder.java
+++ src/main/PrimeCompositeFinder.java
@@ -3,5 +3,12 @@ package main;
 import java.util.ArrayList;
 
 public class PrimeCompositeFinder {
+
+	public static ArrayList<Integer> generateIntegerArray(int min, int max) {
+		ArrayList<Integer> generatedNumbers = new ArrayList<Integer>();
+		for(int i = min; i <= max; i++)
+			generatedNumbers.add(new Integer(i));
+		return generatedNumbers;
+	}
 	
 }
--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -11,18 +11,17 @@ import main.PrimeCompositeFinder;
 public class Tests {
 	
 	@Test
-	public void generateNumbersWithinRange() {
+	public void shouldGenerateNumbersWithinRange() {
 		ArrayList<Integer> wantedNumbers = new ArrayList<Integer>();
 		ArrayList<Integer> generatedNumbers = new ArrayList<Integer>();
 		final Integer min = new Integer(1);
 		final Integer max = new Integer(100);
 		
-		for(int i = min; i <= 100; i++) {
+		for(int i = min; i <= max; i++)
 			wantedNumbers.add(new Integer(i));
-		}
 		
-		generatedNumbers = PrimeCompositeFinder.generate(1, 100);
-		assertEquals(wantedNumbers, generatedNumbers);		
+		generatedNumbers = PrimeCompositeFinder.generateIntegerArray(1, 100);
+		assertEquals(wantedNumbers, generatedNumbers);
 	}
 
 }

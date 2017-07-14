b5c08ba88757d4857b14ddc4d0d93afa4737423f
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-07-15 00:18:19 +0200

    shouldGenerateNumbersWithinRange

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -10,6 +10,19 @@ import main.PrimeCompositeFinder;
 
 public class Tests {
 	
-	
+	@Test
+	public void generateNumbersWithinRange() {
+		ArrayList<Integer> wantedNumbers = new ArrayList<Integer>();
+		ArrayList<Integer> generatedNumbers = new ArrayList<Integer>();
+		final Integer min = new Integer(1);
+		final Integer max = new Integer(100);
+		
+		for(int i = min; i <= 100; i++) {
+			wantedNumbers.add(new Integer(i));
+		}
+		
+		generatedNumbers = PrimeCompositeFinder.generate(1, 100);
+		assertEquals(wantedNumbers, generatedNumbers);		
+	}
 
 }

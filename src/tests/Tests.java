7e0a9c6e9983f7c00c573fc9846a17f9179d70ab
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-07-15 14:33:34 +0200

    shouldPrintCompositeButNotEven

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -42,6 +42,24 @@ public class Tests {
 		assertEquals(wantedNumbers, numbersWithPrimes);
 	}
 	
+	@Test
+	public void shouldPrintCompositeButNotEven() {
+		ArrayList<String> wantedNumbers = new ArrayList<String>();
+		final int min = 1, max = 100;
+		for (Integer i = min; i <= max; i++) {
+			if (isCompositeButNotEven(i))
+				wantedNumbers.add("composite");
+			else
+				wantedNumbers.add(i.toString());
+		}
+		System.out.println(wantedNumbers);
+
+		ArrayList<String> numbersToTest = PrimeCompositeFinder.generateStringNumberArray(min, max);
+		ArrayList<String> numbersWithComposites = PrimeCompositeFinder.findNotEvenComposites(numbersToTest);
+
+		assertEquals(wantedNumbers, numbersWithComposites);
+	}
+	
 	private static boolean isPrime(int n) {
 		if (n == 1)
 			return false;
@@ -51,5 +69,11 @@ public class Tests {
 		}
 		return true;
 	}
-
+	
+	private static boolean isCompositeButNotEven(int n) {
+		if( n == 1) return false;
+		if(isPrime(n)) return false;
+		else if(n%2 == 0) return false;
+		else return true;
+	}
 }

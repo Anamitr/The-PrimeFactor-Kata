95890c2100174e47deb5f663eb16f591c1ccc516
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-07-15 14:21:01 +0200

    generateStringNumberArray

--- src/main/PrimeCompositeFinder.java
+++ src/main/PrimeCompositeFinder.java
@@ -4,4 +4,10 @@ import java.util.ArrayList;
 
 public class PrimeCompositeFinder {
 	
+	public static ArrayList<String> generateStringNumberArray(int min, int max) {
+		ArrayList<String> generatedNumbers = new ArrayList<String>();
+		for(Integer i = min; i <= max; i++)
+			generatedNumbers.add(i.toString());
+		return generatedNumbers;
+	}
 }

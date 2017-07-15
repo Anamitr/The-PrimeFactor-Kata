e8ea8c8acc7207a714a480f9f888038b28b4b6a5
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-07-15 14:22:34 +0200

    findPrimes

--- src/main/PrimeCompositeFinder.java
+++ src/main/PrimeCompositeFinder.java
@@ -10,4 +10,23 @@ public class PrimeCompositeFinder {
 			generatedNumbers.add(i.toString());
 		return generatedNumbers;
 	}
+	
+	public static ArrayList<String> findPrimes(ArrayList<String> numbers) {
+		for(int i = 0; i < numbers.size(); i++) {
+			if(numbers.get(i).matches("^-?\\d+$")) {
+				Integer num = Integer.parseInt(numbers.get(i));
+				if(isPrime(num)) numbers.set(i, "prime");
+			}			
+		}
+		return numbers;
+	}
+	
+	private static boolean isPrime(int n) {
+		if (n == 1) return false;
+	    for(int i=2;i<n;i++) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}	
 }

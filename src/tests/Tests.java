87ba2e8075aad35f59afb924d676a956760e4b77
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-07-15 14:21:42 +0200

    shouldPrintPrimeInsteadOfNumber

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -1,4 +1,5 @@
 package tests;
+
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
@@ -9,19 +10,44 @@ import org.junit.Test;
 import main.PrimeCompositeFinder;
 
 public class Tests {
-	
+
 	@Test
 	public void shouldGenerateNumbersWithinRange() {
 		ArrayList<String> wantedNumbers = new ArrayList<String>();
 		ArrayList<String> generatedNumbers = new ArrayList<String>();
 		final Integer min = new Integer(1);
 		final Integer max = new Integer(100);
-		
-		for(Integer i = min; i <= max; i++)
+
+		for (Integer i = min; i <= max; i++)
 			wantedNumbers.add(i.toString());
-		
+
 		generatedNumbers = PrimeCompositeFinder.generateStringNumberArray(1, 100);
 		assertEquals(wantedNumbers, generatedNumbers);
 	}
 
+	@Test
+	public void shouldPrintPrimeInsteadOfNumber() {
+		ArrayList<String> wantedNumbers = new ArrayList<String>();
+		final int min = 1, max = 100;
+		for(Integer i = min; i <= max; i++) {
+			if(isPrime(i)) wantedNumbers.add("prime");
+			else wantedNumbers.add(i.toString());
+		}
+		System.out.println(wantedNumbers);		
+
+		ArrayList<String> numbersToTest = PrimeCompositeFinder.generateStringNumberArray(min, max);
+		ArrayList<String> numbersWithPrimes = PrimeCompositeFinder.findPrimes(numbersToTest);
+		
+		assertEquals(wantedNumbers, numbersWithPrimes);
+	}
+
+	private static boolean isPrime(int n) {
+		if (n == 1)
+			return false;
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
 }

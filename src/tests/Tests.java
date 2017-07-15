bd3aa83bc471e2902ad5694e50e8491e8cb31d06
Konrad Sochacki <sochacki.konrad@gmail.com>
2017-07-15 14:02:18 +0200

    isPrime

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -39,5 +39,14 @@ public class Tests {
 		
 		assertEquals(wantedNumbers, numbersWithPrimes);
 	}
+	
+	boolean isPrime(int n) {
+		if (n == 1) return false;
+	    for(int i=2;i<n;i++) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}
 
 }
